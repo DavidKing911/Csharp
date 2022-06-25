ae6ad4548c8bd5dc743d2cb6af2d4651a42494eb
Avetyan David <avetyan_david@bk.ru>
2022-06-26 02:38:30 +0700

    Задание_05 выполнено и дополнено задание_08

--- Zadanie_08/Program.cs
+++ Zadanie_08/Program.cs
@@ -4,10 +4,11 @@
 8 -> 2, 4, 6, 8*/
 Console.Write("Введите число: ");
 int N = Convert.ToInt32(Console.ReadLine());
-int a = 1;
+int a = 2;
 Console.Write($"Чётные числа от 1 до {N}: ");
 while(a <= N)
 {
-    if(a % 2 == 0) Console.Write($"{a}, ");
-    a++;
+    if(a % 2 == 0) Console.Write(a);
+    a += 2;
+    if(a <= N) Console.Write(", ");
 }
