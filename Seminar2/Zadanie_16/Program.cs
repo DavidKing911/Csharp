1ab455818540c265d0794a98454ba304420656d8
Avetyan Davidclear <avetyan_david@bk.ru>
2022-07-01 00:09:34 +0300

    Проверка программы

--- Seminar2/Zadanie_16/Program.cs
+++ Seminar2/Zadanie_16/Program.cs
@@ -9,4 +9,4 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 if(num1 == num2 * num2 || num2 == num1 * num1) Console.Write("Да");
-else Console.Write("Нет");
+else Console.Write("Нет"); 
