773dec832be641eab5df494b6b7c0e37d92dd592
Avetyan Davidclear <avetyan_david@bk.ru>
2022-07-06 13:50:16 +0300

    Задание_26 выполнено

---
+++ Seminar4/Zadanie_26/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программу, которая принимает на вход
+число и выдаёт количество цифр в числе.
+Например:
+456 -> 3
+78 -> 2
+89126 -> 5 */
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int count = 0;
+int Num(int number)
+{
+    while (number > 0)
+    {
+        number = number / 10;
+        count += 1;
+    }
+    return count;
+}
+int result = Num(num);
+Console.Write($"Количество цифр числа {num} -> {count}"); 
