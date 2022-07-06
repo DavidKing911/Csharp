741bc9678cd638624f839cf43db16ead26d97c8e
Avetyan Davidclear <avetyan_david@bk.ru>
2022-07-06 13:22:09 +0300

    Задание_25 выполнено

---
+++ Seminar4/Zadanie_25/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите цикл, который принимаетна вход два числа
+(А и В) и возводит число А в натуральную степень В.
+Например:
+3, 5 -> 243 (3^5)
+2, 4 -> 16 */
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+int Function(int a1, int b1)
+{
+    int comp = 1;
+    for (int step = 1; step <= b1; step++)
+    {
+        comp = comp * a1;
+    }
+    return comp;
+}
+int result = Function(a, b);
+Console.Write($"Число {a} в степени {b} -> {result}");
