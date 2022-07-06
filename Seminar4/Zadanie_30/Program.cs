75fb3658b7471f3eaeb73f5cd0067a2a83607378
Avetyan Davidclear <avetyan_david@bk.ru>
2022-07-06 16:17:06 +0300

    Задание_30 выполнено

---
+++ Seminar4/Zadanie_30/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Напишите программу, которая выводит 
+массив из 8 элементов, заполненный 
+нулями и единицами в случайном порядке.
+Например:
+[1, 0, 1, 1, 0, 1, 0, 0]*/
+int [] mas = new int[8];
+Console.Write("[");
+for(int i = 0; i < mas.Length; i++)
+{
+    mas[i] = new Random().Next(0,2);
+    Console.Write(mas[i]);
+    if(i < mas.Length - 1) Console.Write(", ");
+}
+Console.Write("]");
