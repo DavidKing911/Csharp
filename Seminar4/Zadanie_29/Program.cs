d494260a2867b186770395251271ddba7a029ff3
Avetyan Davidclear <avetyan_david@bk.ru>
2022-07-06 15:56:04 +0300

    Задание_29 выполнено

---
+++ Seminar4/Zadanie_29/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая задаёт массив из 8 
+элементов, заполненный псевдослучайными 
+числами и выводит их на экран.
+Например:
+1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33] */
+int [] mas = new int[8];
+Console.Write("[");
+for(int i = 0; i < mas.Length; i++)
+{
+    mas[i] = new Random().Next(1,100);
+    Console.Write(mas[i]);
+    if(i < mas.Length - 1) Console.Write(", ");
+}
+Console.Write("]");
