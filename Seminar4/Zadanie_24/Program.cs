8208c07c1b87a0035889b1e3a49960b302e6cc47
Avetyan Davidclear <avetyan_david@bk.ru>
2022-07-06 12:46:58 +0300

    Задание_24 выполнено

---
+++ Seminar4/Zadanie_24/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите программу, которая принимает на вход
+число (А) и выдаёт сумму чисел от 1 до А.
+Например:
+7 -> 28
+4 -> 10
+8 -> 36 */
+Console.Clear();
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int Sum(int collection)
+{
+    int sum = 0;
+    for (int n = 1; n <= a; n++)
+    {
+        sum += n;
+    }
+    return sum;
+}
+int result = Sum(a);
+Console.Write($"Сумма чисел от 1 до {a} -> {result}");
