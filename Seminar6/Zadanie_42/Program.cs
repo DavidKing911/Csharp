b7bb542b70505c23ab980f0d7861f18f46035b17
Avetyan Davidclear <avetyan_david@bk.ru>
2022-07-11 22:26:14 +0300

    Задания с семинара выполнены

--- Seminar6/Zadanie_42/Program.cs
+++ Seminar6/Zadanie_42/Program.cs
@@ -4,18 +4,6 @@
 45 -> 101101
 3 -> 11
 2 -> 10*/
-/* Console.Write("Введите число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-
-int Reshenie (int number)
-{
-    if (number == 1) return 1;
-    if (number == 0) return 0;
-    return number % 2 + 10 * Reshenie(number / 2);
-}
-
-Console.Write(Reshenie(a)); */
-
 int a = 13;
 
 void Reshenie(int n)
