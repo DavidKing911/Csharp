5ec0be1a4bf238c28b188e8d3283e84cda9c14f0
Avetyan Davidclear <avetyan_david@bk.ru>
2022-07-12 07:27:07 +0300

    Задание_41 выполнено

--- Seminar6/Zadanie_41/Program.cs
+++ Seminar6/Zadanie_41/Program.cs
@@ -3,6 +3,15 @@
 Например:
 0, 7, 8, -2, -2 -> 2
 1, -7, 567, 89, 223-> 3 */
+Console.Clear();
+Console.Write("Введите количество чисел: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[num];
+for (int i = 0; i < num; i++)
+{
+    Console.Write("Введите число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
 
 void Reshenie(int[] arr)
 {
@@ -11,8 +20,7 @@ void Reshenie(int[] arr)
     {
         if(arr[i] > 0) count += 1;
     }
-    Console.Write(count);
+    Console.Write($"{count} - такое количество чисел больше 0");
 }
 
-int[] array = new int[4]{3,4,-2,-1};//[размер массива]{числа}
 Reshenie(array);
